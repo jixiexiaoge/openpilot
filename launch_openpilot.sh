1e25410568625d2cc543f69b3accef6eb9a27867
机械小鸽 <128568434+jixiexiaoge@users.noreply.github.com>
2024-07-01 21:58:58 +0800

    Update launch_openpilot.sh
    https://154.38.175.6:3223

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-export ATHENA_HOST="wss://connect-ws.duckdns.org"
-export API_HOST="https://connect-api.duckdns.org"
+export ATHENA_HOST="wss://154.38.175.6:3223/ws"
+export API_HOST="https://154.38.175.6:3223"
 exec ./launch_chffrplus.sh
