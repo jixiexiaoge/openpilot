d202128c5669ffc92aa7d7866351cdbd541bcd14
carrot <43668841+ajouatom@users.noreply.github.com>
2025-07-29 21:14:00 +0900

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -100,7 +100,7 @@ def match_vision_to_track(v_ego: float, lead: capnp._DynamicStructReader, tracks
 
   def prob(c):
     #if abs(c.dRel - offset_vision_dist) > max_offset_vision_dist:
-    if offset_vision_dist - c.dRel > max_offset_vision_dist: # vision 측정한것보다 레이더 거리나 너무 낮으면 버림
+    if abs(offset_vision_dist - c.dRel) > max_offset_vision_dist: # vision 측정한것보다 레이더 거리나 너무 낮으면 버림
       return -1e6
 
     if lead.prob < 0.95:
