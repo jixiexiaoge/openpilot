f59de023c44895820d03bd32c9b5bd9b6c3b27f2
carrot <43668841+ajouatom@users.noreply.github.com>
2026-01-09 09:08:12 +0900

    carrot_functions.py 업데이트

--- selfdrive/carrot/carrot_functions.py
+++ selfdrive/carrot/carrot_functions.py
@@ -522,7 +522,7 @@ class DrivingModeDetector:
         # ---- 진입 조건(OR로 묶기) ----
         enter = (
             (distance <= self.distance_threshold and lead_speed <= self.speed_threshold) or
-            (lead_speed < 5 and lead_accel < 0.2 and my_speed > 1.0)
+            (lead_speed < 5 and lead_accel < 0.2 and my_speed > 1.0 and distance > 0)
         )
 
         # ---- 탈출 조건(더 보수적으로) ----
