4100d7995240c52db4ead7a1563344569544c4f3
ajouatom <ajouatom@gmail.com>
2026-02-02 20:35:55 +0900

    fix..

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -79,7 +79,7 @@ def enable_xiaoge_data(started, params, CP: car.CarParams) -> bool:
   return params.get_bool("ShareData")
 
 def enable_webrtc(started, params, CP: car.CarParams) -> bool:
-  return False
+  return True
 
 def c3x_lite(started: bool, params: Params, CP: car.CarParams) -> bool:
   return started and params.get_bool("HardwareC3xLite")
